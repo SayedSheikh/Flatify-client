5bc577320836edd9f490a57a2f7829e066a323ca
Sayed Sheikh <sayedsheikh9@gmail.com>
2025-05-23 19:37:27 +0600

    review modal added

--- src/Components/Reviews/Reviews.jsx
+++ src/Components/Reviews/Reviews.jsx
@@ -1,5 +1,7 @@
-import React from "react";
+import React, { use, useState } from "react";
+import toast from "react-hot-toast";
 import { FaStar } from "react-icons/fa";
+import { AuthContext } from "../../Contexts/AuthContext";
 
 const reviews = [
   {
@@ -43,13 +45,55 @@ const reviews = [
 ];
 
 const Reviews = () => {
+  const { user } = use(AuthContext);
+  console.log(user);
+  const [review, setReview] = useState("");
+  const [area, setArea] = useState("");
+  const [rating, setRating] = useState(1); // default rating
+  const [userReview, setUserReview] = useState([]);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (!user) {
+      toast.error("logIn first to add review");
+    }
+    if (review.trim() !== "") {
+      document.getElementById("my_modal_5").close();
+      // console.log(review, rating);
+      const obj = {
+        rating,
+        review,
+        area,
+        name: user?.displayName || "",
+      };
+      setUserReview(obj);
+      setReview("");
+      setRating(1);
+      setArea("");
+      toast.success("Review Added Successfully !!");
+      // console.log(userReview);
+    } else {
+      // alert("please write your comment");
+      toast.error("please write your comment");
+    }
+  };
+  const handleReview = () => {
+    if (user) {
+      document.getElementById("my_modal_5").showModal();
+    } else {
+      // alert("Install the app first");
+      toast.error("Install the app first");
+    }
+  };
   return (
     <section className="my-16 max-w-[1300px] mx-auto px-4">
       <div className="flex flex-col sm:flex-row justify-between items-center mb-6 gap-2">
         <h2 className="text-3xl font-bold text-secondary self-start">
           What Our Users Say
         </h2>
-        <button className="btn btn-outline btn-primary self-end">
+        <button
+          onClick={handleReview}
+          className="btn btn-outline btn-primary self-end w-full sm:w-fit mt-4 sm:mt-0">
           Give Your Review
         </button>
       </div>
@@ -75,6 +119,53 @@ const Reviews = () => {
           </div>
         ))}
       </div>
+
+      {/* Open the modal using document.getElementById('ID').showModal() method */}
+
+      <dialog id="my_modal_5" className="modal modal-bottom sm:modal-middle">
+        <div className="modal-box">
+          <h3 className="font-bold text-lg mb-[20px]">Give your feedback</h3>
+          <div className="flex gap-[10px]">
+            <p>Rating</p>
+            <div className="rating rating-sm mb-[10px]">
+              {[1, 2, 3, 4, 5].map((value) => (
+                <input
+                  key={value}
+                  type="radio"
+                  name="rating"
+                  className="mask mask-star-2 bg-orange-400"
+                  aria-label={`${value} star`}
+                  checked={rating === value}
+                  onChange={() => setRating(value)}
+                />
+              ))}
+            </div>
+          </div>
+          <div className="my-[5px]">
+            <p>Area</p>
+            <input
+              type="text"
+              value={area}
+              onChange={(e) => setArea(e.target.value)}
+              className="border border-primary rounded-[3px]"
+            />
+          </div>
+          <textarea
+            name="review"
+            placeholder="Write your review here..."
+            className="textarea textarea-primary w-full"
+            rows={5}
+            value={review}
+            onChange={(e) => setReview(e.target.value)}
+          />
+          <div className="modal-action">
+            {/* if there is a button in form, it will close the modal */}
+            <button onClick={handleSubmit} className="btn btn-primary">
+              Submit
+            </button>
+          </div>
+        </div>
+      </dialog>
     </section>
   );
 };
